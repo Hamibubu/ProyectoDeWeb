adfa2fb452887d31085238ccfaec1fc4acbe9699
Abraham De León <abraham.deleon@iteso.mx>
2023-11-12 12:50:24 -0600

    Develop

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,10 +10,19 @@ class UsersController {
     async crearusuario(req,res) {
         const user = new Usuario(req.body);
         try{
-            await user.save();
-            res.status(201).send(user);
+            await user.save((err) => {
+                if (err) {
+                    if (err.code === 11000) {
+                        res.status(400).send("El email ya existe en la base");
+                    } else {
+                        res.status(500).send("Error interno");
+                    }
+                  } else {
+                    res.status(201).send(user);
+                  }
+            });
         } catch(err) {
-            res.status(500).send(err);
+            res.status(500).send("Error interno");
         }
     }
 
--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     genres: { type: String, required: true },
     albumfav: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: { type:String, required: true },
     phone: { type:String, required: true }
 });
