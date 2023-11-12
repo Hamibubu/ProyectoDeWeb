d325ff467a853527b1443a91ff11f58313d09696
Emiliano Arroyo <113309542+EmilianoArroyo@users.noreply.github.com>
2023-11-12 14:39:28 -0600

    actualizacion models db unique email

--- src/models/artistsModel.js
+++ src/models/artistsModel.js
@@ -4,7 +4,7 @@ const artistSchema = new mongoose.Schema({
     name: { type: String, required: true },
     username: { type: String, required: true },
     genre: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     description: { type: String, required: true },
     Influences: { type:String, required: true }
