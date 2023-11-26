61c3e8aa9cb900b3d5eb4f6e56b24b1282642249
EmilianoArroyo <emiliano.arroyo@iteso.mx>
2023-11-25 18:41:22 -0600

    handling 404

--- index.js
+++ index.js
@@ -70,3 +70,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/views/index/index.html'));
   });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/views/errors/404.html'));
+  });
